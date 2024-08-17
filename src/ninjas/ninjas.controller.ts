82925e5c5691a3e7d445615b9c565ca68ccb0549
Syed Arsalan <sya@4fo.de>
2024-08-17 14:47:19 +0200

    added ninja module and controller dummy routes

---
+++ src/ninjas/ninjas.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateNinjaDto } from './create-ninja.dto';
+import { UpdateNinjaDto } from './update-ninja.dto';
+
+@Controller('ninjas')
+export class NinjasController {
+
+    @Get()
+    getNinjas(@Query('type') type: string) {
+        return [{ type }];
+    }
+
+    @Get(':id')
+    getOneNinja(@Param('id') id: string) {
+        return {
+            id,
+        }
+    }
+
+    @Post()
+    createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+        return {
+            name: createNinjaDto.name
+        };
+    }
+
+    @Put(':id')
+    updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+        return {
+            id,
+            name: updateNinjaDto
+        }
+    }
+}
